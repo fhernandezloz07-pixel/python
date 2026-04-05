b1f466d80e627691d37346c9cca72b6a4eba931d
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-04-05 18:50:42 -0500

    finished problem 3 first Duplicate

--- CodePath/Units/Unit 3/Session 1.py/Version 3.py
+++ CodePath/Units/Unit 3/Session 1.py/Version 3.py
@@ -37,4 +37,48 @@ def rotate_left(s, n):
 
 s = "rotation"
 print(rotate_left(s, 2))
-# What did you lea
+# What did you learn? String interpolation with indexing
+
+print("-------------------------------------------------------")
+# Problem 3: First Duplicate
+    # Write a function first_repeated_char() that takes in a string s as a parameter and 
+	# returns the index of the first repeated character in the string. 
+	# Uppercases and lowercase letters are considered different characters,
+	# and the function returns None if there are no repeated characters. 
+'''
+U:
+Based on a string s return the index of the first repeated character in the string 
+Return none if there are no repeated characters
+P:
+- brainstorm: 
+    We will need a lot of loops here and I could possibly use a list but 
+    with a list do you want to save the character of the index itself!?!
+
+	define a function with the appropiate parameters 
+	initialize a repetitions dictionary
+	for character in s: 
+	if s not in dictionary:
+	add it and initialize to 1
+	else: 
+	add 1 
+	
+	for the first key in the dictionary: (I think here you have to use )
+	
+'''
+def first_repeated_char(s):
+	rep_dictionary = {}
+	for char in s:
+		if s not in rep_dictionary:
+			rep_dictionary[s] = len(s) # we want the value of the key to be the index of the character in the string 
+		else:
+			rep_dictionary[s] = len(s)
+	value_index = # return the value of the first key
+
+s = "hello world"
+s2 = "aAbBCC"
+s3 = "abcdefg"
+
+print(first_repeated_char(s))
+print(first_repeated_char(s2))
+print(first_repeated_char(s3))
+		
