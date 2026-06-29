a9401fb440744ba15588dc919b0c6c2161def54c
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-06-29 14:46:12 -0500

    more arithmetic

---
+++ BroCode/5_Arithmetic&Math/02_Math_related_functions.py
@@ -0,0 +1,12 @@
+
+x = 3.14
+y = 4
+z = 5
+
+result = round(x) # Rounding to the nearest whole number 
+result = abs(y) # taking the absolute value of a number
+result = pow(4, 3) # first number to the power of the second
+result = max(x, y, z) # find the max in all those numbers
+result = min(x, y, z) # find the min in all those numbers
+
+print(result)
