dfcff643b3ad7f54965ac8a1df73003d71fe9714
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-06-28 21:57:55 -0500

    madlibs game using python

---
+++ BroCode.py/4_MadlibsGame/01_game.py
@@ -0,0 +1,14 @@
+# Madlibs game
+# word game where you create a story
+# by filling in blanks with random words
+
+adjective1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun (person, place, or thing): ")
+adjective2 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb ending with 'ing' (action): ")
+adjective3 = input("Enter an adjective (description): ")
+
+print(f"Today I went to a {adjective1} zoo.") # adjective ->word to describe something
+print(f"In an exhibit I saw a {noun1}") # noun ->person, place, or thing
+print(f"{noun1} was {adjective2} and {verb1}") # verb -> an action such as running or eating
+print(f"I was {adjective3}!")
