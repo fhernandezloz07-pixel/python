6051ec4c704448354eba2bf0c7f2cdd745ebe17a
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-06-28 21:23:54 -0500

    example python

--- BroCode.py/3_UserInput/01_examples.py
+++ BroCode.py/3_UserInput/01_examples.py
@@ -1,3 +1,11 @@
 # input() = A function that prompts the user to enter data
 #           Returns the entered data as a string
 
+name = input("What is your name?: ")
+age = int(input("How old are you?: ")) # convert to int so we can increment
+
+age += 1
+
+print(f"Hello {name}!")
+print("HAPPY BIRTHDAY!") # use f string only if you want to insert variables
+print(f"You are {age} years old")
