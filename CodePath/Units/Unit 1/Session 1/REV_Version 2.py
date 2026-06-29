c5e5c120ff65d0429233c27d759b6c55b4330cdf
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-06-29 14:37:16 -0500

    more brocode coding

---
+++ CodePath/Units/Unit 1/Session 1/REV_Version 2.py
@@ -0,0 +1,22 @@
+# Problem 1: Hello User!
+def greet_user(name):
+    print(f"Hello {name}")
+
+student_1 = "Michael"
+greet_user(student_1)
+print("__________________________________________________________")
+
+
+# Problem 2: Calculate Difference
+def difference(a, b):
+    return a - b 
+
+diff = difference(8, 3)
+print(f"diff = {diff}")
+print("__________________________________________________________")
+
+# problem 3: List Concatenation
+def concatenate_list(nums):
+    
+
+
