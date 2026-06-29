6a6894ad3806c2028d703dfb16b77c8c5b549f11
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-06-29 14:41:33 -0500

    basic arithmetic

--- BroCode/5_Arithmetic&Math/01_examples.py
+++ BroCode/5_Arithmetic&Math/01_examples.py
@@ -1,1 +1,24 @@
-#  
+
+friends = 10
+
+# friends = friends + 1
+friends += 1 # augmented assignment perators
+
+# friends = friends - 2
+friends -= 2
+
+# friends = friends * 3
+friends *= friends
+
+#friends = friends / 2
+friends /= 2
+
+# friends = friends ** 2 -> this is for exponents
+friends **= 2
+
+#       -> modulus is kinda like the remainder, 
+#       find if the number is even or odd with this!
+remainder = friends % 3
+
+print(friends)
+
