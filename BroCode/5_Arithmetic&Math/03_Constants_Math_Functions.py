e41d9695e6f5b7475d07db422957332f10df05c8
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-06-29 14:50:29 -0500

    unsing more math functions

---
+++ BroCode/5_Arithmetic&Math/03_Constants_Math_Functions.py
@@ -0,0 +1,14 @@
+import math # import the math module
+
+
+print(math.pi)
+print(math.e)
+
+x = 9
+result = math.sqrt(x) # finding the square root of 9, which is 3
+print(result)
+
+y = 9.1
+result = math.ceil(y) # ceiling function, round a floating point number up
+result = math.floor(y) # rounds a float down
+
