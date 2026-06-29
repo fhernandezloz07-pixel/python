aebdacae5907d0f1c1b1434cd270792450681609
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-06-29 16:06:14 -0500

    if statements

---
+++ BroCode/6_ifStatements/01_examples.py
@@ -0,0 +1,9 @@
+# if = Do some code only IF some condition is True
+#      Else do something else
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are now signed up!")
+else: 
+    print("You must be 18+ to sign up")
