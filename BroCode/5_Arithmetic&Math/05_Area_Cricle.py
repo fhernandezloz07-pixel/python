0c7e293b4540289ef2723d8d4561906b0a2bf851
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-06-29 15:00:57 -0500

    Area of circle

---
+++ BroCode/5_Arithmetic&Math/05_Area_Cricle.py
@@ -0,0 +1,9 @@
+import math
+# Calculating the area of a circle
+#       -> we need pi * radius squared
+
+radius = float(input("Enter the radius of a circle: "))
+
+area = math.pi * pow(radius, 2)
+
+print(f"The area of the circle is: {round(area, 2)}cm^2")
