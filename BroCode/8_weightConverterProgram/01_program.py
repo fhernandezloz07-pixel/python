0db9739a9fe307f9a2072d726d10ce0365178e97
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-07-13 11:25:35 -0500

    tests

---
+++ BroCode/8_weightConverterProgram/01_program.py
@@ -0,0 +1,15 @@
+# Python weight converter
+
+weight = float(input("Enter your weight: "))
+unit = input("Kilograms or pounds? (K or L): ")
+
+if unit == "K":
+    weight = weight * 2.205
+    unit = "Lbs."
+elif unit == "L":
+    weight = weight / 2.205
+    unit = "Kgs."
+else:
+    print(f"{unit} was not valid");
+
+print(f"Your weight is: {weight} {unit}")
