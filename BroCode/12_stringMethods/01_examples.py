d6f2075f4ad47d36e696b7515c4f3de7177ac2de
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-07-15 10:34:33 -0500

    changes

--- BroCode/12_stringMethods/01_examples.py
+++ BroCode/12_stringMethods/01_examples.py
@@ -1,6 +1,8 @@
 
 name = input("Enter your full name: ")
 
-result = len(name)
+# result = len(name)
+result = name.find(" ") # will return the first occurence of a given character
+result = name.rfind # r reverse because here we are finding the last occurence of a given character
 
 print(result)
