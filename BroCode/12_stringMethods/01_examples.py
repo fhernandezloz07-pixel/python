3a84523159b16c02d2f504f178f625cd7631acec
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-07-15 11:00:56 -0500

    string methods

--- BroCode/12_stringMethods/01_examples.py
+++ BroCode/12_stringMethods/01_examples.py
@@ -1,6 +1,6 @@
 
 # name = input("Enter your full name: ")
-phone_number = input("Enter your phone number: ")
+# phone_number = input("Enter your phone number: ")
 
 # result = len(name)
 # result = name.find(" ") # will return the first occurence of a given character
@@ -11,8 +11,9 @@ phone_number = input("Enter your phone number: ")
 # result = name.isdigit() # return either true or false if the string contains only digits
 # result = name.isalpha() # return a boolean true or false depending if the string contains only letters
 # result = phone_number.count("-") # count how many characters are within a string
-result = phone_number.replace("-", " ") # replacing one character with another
+# result = phone_number.replace("-", " ") # replacing one character with another
 
-print(result)
+# print(result)
 
 # if you ever need help deciding what to use or cant remember you can use the following
+print(help(str))
