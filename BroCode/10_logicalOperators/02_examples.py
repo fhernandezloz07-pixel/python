532b81a3aa9bdbfb8f5984d425459b265d6ccab6
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-07-15 10:02:36 -0500

    and operator

---
+++ BroCode/10_logicalOperators/02_examples.py
@@ -0,0 +1,13 @@
+
+temp = 20
+is_sunny = True
+
+if temp >= 28 and is_sunny:
+    print("It is HOT outside 🥵")
+    print("It is SUNNY ☀️")
+elif temp <= 0 and is_sunny:
+    print("It is COLD outside 🥶")
+    print("It is SUNNY ☀️")
+elif 28 > temp > 0 and is_sunny:
+    print("It is WARM outside 😊")
+    print("It is SUNNY ☀️")
