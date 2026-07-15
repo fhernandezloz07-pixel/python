d6f0eaf1c74334518980bbd0b3b46d3835a1297f
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-07-15 10:55:31 -0500

    count method

--- BroCode/12_stringMethods/01_examples.py
+++ BroCode/12_stringMethods/01_examples.py
@@ -1,8 +1,15 @@
 
-name = input("Enter your full name: ")
+# name = input("Enter your full name: ")
+phone_number = input("Enter your phone number: ")
 
 # result = len(name)
-result = name.find(" ") # will return the first occurence of a given character
-result = name.rfind # r reverse because here we are finding the last occurence of a given character
+# result = name.find(" ") # will return the first occurence of a given character
+# result = name.rfind("o") # r reverse because here we are finding the last occurence of a given character, and returns -1 when the character is not found
+# name = name.capitalize() # capitalize the first letter of a string
+# name = name.upper() # all uppercase in a string
+# name = name.lower() # all lowercase in a string
+# result = name.isdigit() # return either true or false if the string contains only digits
+# result = name.isalpha() # return a boolean true or false depending if the string contains only letters
+result = phone_number.count("-") # count how many characters are within a string
 
 print(result)
