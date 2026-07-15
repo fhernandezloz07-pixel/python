ad49138aa89bab08fecb3b55afb9538535ceb78f
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-07-15 10:22:03 -0500

    conditional expressions

--- BroCode/11_conditionalExpressions/01_examples.py
+++ BroCode/11_conditionalExpressions/01_examples.py
@@ -1,1 +1,4 @@
 # conditional expressions = A one-line shortcut for the if-else statement (ternary operator)
+#                           Print or assing one of the two values based on a condition
+#                           X if condition else Y
+
