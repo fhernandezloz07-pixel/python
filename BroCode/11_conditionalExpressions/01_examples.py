961d9df090c9794d9b24397df691171ead3f507e
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-07-15 10:22:23 -0500

    conditional expressions

--- BroCode/11_conditionalExpressions/01_examples.py
+++ BroCode/11_conditionalExpressions/01_examples.py
@@ -2,3 +2,21 @@
 #                           Print or assing one of the two values based on a condition
 #                           X if condition else Y
 
+num = 5
+a = 6
+b = 7
+age = 13
+temp = 30
+user_role = "admin"
+
+# print("Positive" if num > 0 else "Negative")
+#result = "EVEN" if num % 2 == 0 else "ODD"
+# max_num = a if a > b else b
+# min_num = a if a < b else b
+# status = "Adult" if age >= 18 else "Child"
+# weather = "HOT" if temp > 20 else "COLD"
+access_level = "full access" if user_role == "admin" else "Limitted Acess"
+
+
+
+print(access_level)
