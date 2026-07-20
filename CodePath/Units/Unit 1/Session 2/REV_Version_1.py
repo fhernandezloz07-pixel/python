ab4486af651d9638e744a63fb444163d3abe95bb
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-07-20 15:26:28 -0500

    problem 1

---
+++ CodePath/Units/Unit 1/Session 2/REV_Version_1.py
@@ -0,0 +1,11 @@
+# Problem 1: Print list
+#            go through the list with a for loop and print each item in the list
+#            you dont have to use the print function bc you already used a print in the for loop
+def print_list(lst):
+    for item in lst:
+        print(item)
+
+lst = ["squirtle", "gengar", "charizard", "pikachu"]
+print_list(lst)
+
+
