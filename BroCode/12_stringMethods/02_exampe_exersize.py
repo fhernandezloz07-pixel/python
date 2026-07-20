0a859461fd4e703b606ac7dec3b10842e227c6fe
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-07-20 14:25:00 -0500

    fixing and edition

--- BroCode/12_stringMethods/02_exampe_exersize.py
+++ BroCode/12_stringMethods/02_exampe_exersize.py
@@ -7,11 +7,11 @@ username = input("Enter a username: ")
 
 
 
-if len(username) > 12:
+if len(username) > 12: # checks the length of the username
     print("Your username can't be more than 12 characters")
-elif not username.find(" ") == -1:
+elif not username.find(" ") == -1: #  -1 if no spaces are found
     print("Your username can't contain spaces")
-elif not username.isalpha():
+elif not username.isalpha(): # checks if the username has anything thats not a letter
     print("Your username can't contain numbers")
 else:
     print(f"Welcome {username}")
