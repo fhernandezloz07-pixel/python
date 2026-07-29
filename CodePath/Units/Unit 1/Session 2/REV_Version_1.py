636a3438abb63793377f3e091585ed29a8c3215d
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-07-29 02:16:13 -0500

    coding

--- CodePath/Units/Unit 1/Session 2/REV_Version_1.py
+++ CodePath/Units/Unit 1/Session 2/REV_Version_1.py
@@ -8,4 +8,45 @@ def print_list(lst):
 lst = ["squirtle", "gengar", "charizard", "pikachu"]
 print_list(lst)
 
+print("---------------------------------------------------------------")
+# Problem 2: Print Double List Items
+def double(lst):
+    for num in lst:
+        num *= 2
+        print(num)
+
+lst = [1, 2, 3]
+double(lst)
+
+print("---------------------------------------------------------------")
+# problem 3: Return Double List
+def doubled(lst):
+    list = [] # initialize a new empty list
+    for num in lst:
+        num *= 2
+        list.append(num)
+    return list
+
+lst = [1,2,3]
+new_lst = doubled(lst)
+print(new_lst)
+
+print("---------------------------------------------------------------")
+# Problem 4: Flip Signs
+def flip_sign(lst):
+    list = []
+    for num in lst:
+        num *= -1
+        list.append(num)
+    return list
+
+lst = [1,-2,-3,4]
+flipped_lst = flip_sign(lst)
+print(flipped_lst)
+
+print("---------------------------------------------------------------")
+# Problem 5: Max Difference
+def max_difference(lst):
+    for num in lst: 
+        if num - 1 
 
